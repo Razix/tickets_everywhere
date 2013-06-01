089568d8d9bf564f096d996f5ebd4d8d641d8008
Razix <uscreamrazix@gmail.com>
2013-06-01 11:57:22 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TicketsEverywhere::Application.config.secret_token = 'ae870cea8b0672052ff0d7b4a286f1a6aa905b53699d6730670af691c4662bad92e24ebdb133d3455c2b0025e3ef1a65efee28a00b8c0380925770334ae4b990'
